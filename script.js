07d0fcd4fb1008d05f20076ff3b64f9d3d02d9c4
arquitectosalvadorgonzalez-bit <arquitecto.salvadorgonzalez@gmail.com>
2026-07-22 21:26:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const api =
+"https://script.google.com/macros/s/AKfycbwWyQ93S6-Mj9L6VtsgrP1aN0VL7drhWjr7rzUVIWJzw6tK5wyhWO7yavkvrrnNs0134Q/exec";
 
+fetch(api)
+.then(r => r.json())
+.then(players => {
+
+    document.getElementById("playerCount").innerHTML =
+        `${players.length} Registered Players`;
+
+    let html = "";
+
+    players.forEach((p,i)=>{
+
+        html += `
+        <div class="player-card">
+
+            <strong>${i+1}. ${p.nombre}</strong><br>
+
+            HC: ${p.handicap}<br>
+
+            ${p.campo}
+
+        </div>
+        `;
+
+    });
+
+    document.getElementById("playersList").innerHTML = html;
+
+});
